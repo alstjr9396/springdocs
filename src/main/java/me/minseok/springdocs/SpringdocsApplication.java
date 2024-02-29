8fc2819aa425153dc6b38d75edb02f4e983cd463
kang <minseokkang95@gmail.com>
2024-02-29 12:15:51 +0900

    init commit

---
+++ src/main/java/me/minseok/springdocs/SpringdocsApplication.java
@@ -0,0 +1,13 @@
+package me.minseok.springdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdocsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/minseok/springdocs/SpringdocsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.minseok.springdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdocsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
