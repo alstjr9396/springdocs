4282a6977b87e879fcde2ece03df3dc1abdaa9ae
kang <minseokkang95@gmail.com>
2024-03-11 20:54:15 +0900

    logback 적용

--- src/main/java/me/minseok/springdocs/global/exception/GlobalExceptionHandler.java
+++ src/main/java/me/minseok/springdocs/global/exception/GlobalExceptionHandler.java
@@ -1,19 +1,23 @@
 package me.minseok.springdocs.global.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BadRequestException.class)
     protected ResponseEntity<ErrorResponse> handleHateoasException(BadRequestException e) {
+        log.warn(e.getErrorCode().getMessage(), e);
         return ErrorResponse.toResponseEntity(e.getErrorCode());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     protected ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        log.warn(e.getMessage(), e);
         return ErrorResponse.toResponseEntity(ErrorCode.BAD_REQUEST);
     }
 }
